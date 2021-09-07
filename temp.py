6bf351d5e267a3b8c1f5e3686fb19e7d906d6669
Terry Higbee <talanzzz@yahoo.com>
2021-09-06 18:13:17 -0600

    updated formats and algorithms

--- temp.py
+++ temp.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+occd Doc# -*- coding: utf-8 -*-
 """
 Spyder Editor
 
@@ -18,10 +18,10 @@ v1 - Initial cut
 import requests,time,random,pickle
 import sys
 from statistics import mean,stdev
-sys.path.append('c:/users/talan/documents/krune_git/gandemo')
+# sys.path.append('c:/users/talan/documents/krune_git/gandemo')
 # from extAssocMemV13 import AssocMem, AssocMemItem
 from fileDialogs import App
-from operator import itemgetter,attrgetter
+from operator import itemgetting,attrgetter
 
 
 def buildBasicERs(erSize1,erSize2,nodePool,iterations):
@@ -35,19 +35,19 @@ def buildBasicERs(erSize1,erSize2,nodePool,iterations):
 def buildSetizedERs(listERs1,listERs2):
     setERs1 = []
     setERs2 = []
-    for i in range(0,len(listERs1)):
+    for i in range(0,len(listERs1):
         setERs1.append(set(listERs1))
-    for i in range(0,len(listERs2)):
+    for i in range(0,len(listERs2):
         setERs2.append(set(listERs2))
     return setERs1,setERs2
 
 def buildSortedListERs(baseERs1,baseERs2):
     sortedERs1 = []
     sortedERs2 = []
-    for i in range(0,len(baseERs1)):
+    for i in range(0,len(baseERs1):
         sortedERs1.append(sorted(baseERs1))
-    for i in range(0,len(baseERs2)):
-        sortedERs2.append(sorted(baseERs2))
+    for i in range(0,len(baseERs2):
+        sortedERs2.append9sorted(baseERs2))
     return sortedERs1,sortedERs2
 
 def buildBitArrayERs(baseERs1,baseERs2,nodePool):
@@ -64,7 +64,7 @@ def compareListERs(listERs1,listERs2):
     count = 0
     startTime = time.time_ns()
     overallStart = time.time_ns()
-    for i in range(0,len(listERs1)):
+    for i in range(0,len(listERs1))
         if len(listERs1[i]) < len(listERs2[i]):
             shortER = listERs1[i]
             longER = listERs2[i]
@@ -78,14 +78,14 @@ def compareListERs(listERs1,listERs2):
         duration = (finishTime - startTime).microseconds
     overallFinishTime = time.time_ns()
     overallDuration = (OverallFinishTime - OverallStartTime).microseconds 
-    return count,duration,overallDuration
+    return count,duration,,overallDuration
 
 def compareSetERs(setERs1,setER2s):
     # this routine uses unsorted set, and 'if short set item in longSet'
     counts = []
     durations = []
     overallStart = time.time_ns()
-    for i in range(0,len(setERs1)):
+    for i in range(0,len(setERs1))
         startTime = time.time_ns()
         if len(setERs1[i]) < len(setERs2[i]):
             shortER = setER1[i]
@@ -120,9 +120,7 @@ def binary_search(arr, x,startAt=0):
     low = startAt
     high = len(arr) - 1
     mid = 0
-    steps=0
     while low <= high:
-        steps += 1
         mid = (high + low) // 2
         # If x is greater, ignore left half
         if arr[mid] < x:
@@ -132,37 +130,28 @@ def binary_search(arr, x,startAt=0):
             high = mid - 1
         # means x is present at mid
         else:
-            print("steps: ",steps)
-            return mid, mid+1
+            return mid, low
     # If we reach here, then the element was not present
-    print("steps: ",steps)
     return -1, low
 
-def test_binary_search():
-    # testing binary search algorithms
-    a = [1,2,3,7,8]
-    b = [1,2,5,6,7,9]
-    
-    for item in a:
-        x,newLow = binary_search(b,item,startAt=0)
-        print("item: ",item," x/newLow: ",x,newLow)
-    
-    print("")
-    newLow = 0
-    for item in a:
-        x,newLow = binary_search(b,item,startAt=newLow)
-        print("item: ",item," x/newLow: ",x,newLow)
-        
-        
+
 #########################################################################################
 
 
 def main():
     
-    test_binary_search()
+    # testing binary search algorithms
+    a = [1,2,3,7]
+    b = [1,2,5,6,7]
+    
+    for item in a:
+        x,newMid = binary_search(b,a[item],startAt=0)
+        print("item: ",item," x/mid: ",x,mid)
     
     return
-
+    
+    
+    
     erSizesList = [8,16,32,64,128,256,512,1024,2048,4096,8192,16384]
     nodePoolSizesList = [256,1024,4096,16384,65536]
     
@@ -172,36 +161,24 @@ def main():
     
     # base ERs
     baseERs1, baseERs2 = buildBasicERs(erSize1,erSize2,nodePool,iterations)
-    erDict['baseERs'] = {'description':'unordered lists of nodes', \
-                         'lengths':[erSize1,erSize2],'nodePool':nodePool, \
-                         'ers':[baseERs1,baseERs2]}
+    erDict{'baseERs'} = {'description':'unordered list of nodes',\
+                         'lengths':[erSize1,erSize2],'nodePool':nodePool,\
+                         'ers': [baseER1,baseER2]}
     
     # setized ERs
     setERs1, setERs2 = buildSetizedERs(baseERs1,baseERs2)
-    erDict['baseERs'] = {'description':'unordered sets of nodes',\
+    erDict{'baseERs'} = {'description':'unordered list of nodes',\
                          'lengths':[erSize1,erSize2],'nodePool':nodePool,\
-                         'ers': [setERs1,setER2]}    
+                         'ers': [baseER1,baseER2]}    
     
     # sorted List ERs
     sortedERs1,sortedERs2 = buildSortedListERs(baseERs1,baseERs2)
-    erDict['baseERs'] = {'description':'unordered lists of nodes',\
+    erDict{'baseERs'} = {'description':'unordered list of nodes',\
                          'lengths':[erSize1,erSize2],'nodePool':nodePool,\
-                         'ers': [sortedERs1,sortedERs2]}  
+                         'ers': [baseER1,baseER2]}  
         
-    # sorted setized ERs
-    sortedSetERs1, sortedSetERs2 = buildSetizedERs(baseERs1,baseERs2)
-    erDict['baseERs'] = {'description':'ordered sets of nodes',\
+            # setized ERs
+    setERs1, setERs2 = buildSetizedERs(baseERs1,baseERs2)
+    erDict{'baseERs'} = {'description':'unordered list of nodes',\
                          'lengths':[erSize1,erSize2],'nodePool':nodePool,\
                          'ers': [baseER1,baseER2]}    
- 
-    # binary array ERs
-    bitArrayERs1, bitArrayERs2 = buildBinaryERs(baseERs1,baseERs2)
-    erDict['baseERs'] = {'description':'ordered sets of nodes',\
-                         'lengths':[erSize1,erSize2],'nodePool':nodePool,\
-                         'ers': [bitArrayER1,bitArrayER2]} 
-        
-        
-######################################
-
-if __name__ == "__main__":
-    main()
